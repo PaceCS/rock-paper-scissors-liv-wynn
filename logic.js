49251b82fe1bf3585791657a4c9d4027850e777c
Programming <programming@us-physics-cart-15.local>
2018-05-09 10:33:32 -0400

    RPS finished

--- logic.js
+++ logic.js
@@ -1,58 +1,41 @@
-
 // This array will hold the options for the game
 // Fill in your options below
-const options = [/*option 1*/, /*option 2*/, /*option 3*/];
+const options = ['Rock', 'Paper', 'Scissors'];
 
 // This function takes in the userChoice and then prints the results to the console.
 function playGame(userChoice) {
-
     // First remind the user of his/her choice
-    console.log(/*Put text here*/);
+    console.log('You chose ' + userChoice + '.');
 
     // Now choose a random number between 0 and 2
-    const rand = /*Fill in the code for a random value between 0 and 2*/;
+    const rand = Math.floor(Math.random() * 3);
 
     // Then use this random number to give the computer a choice
     const compChoice = options[rand];
 
     // Now log the computer's choice to the console.
-    console.log(/*Put text here*/);
+    console.log('The computer chose ' + compChoice + '.');
 
-    // Now determine the winner using if/else statements
-    /*
-    |
-    |
-    This will take several if/else statements, so it will take multiple lines.
-    |
-    |
-    */
-
+    if (userChoice == compChoice) {
+        console.log('You tied');
+    } else if (compChoice == 'Rock') {
+        if (userChoice == 'Paper') {
+            console.log('You win. Paper beats Rock');
+        } else {
+            console.log('You lose. Rock beats Scissors.');
+        }
+    } else if (compChoice == 'Paper') {
+        if (userChoice == 'Rock') {
+            console.log('You lose. Paper beats Rock.');
+        } else {
+            console.log('You win. Scissors beats Paper.');
+        }
+    } else if (userChoice == 'Paper') {
+        console.log('You lose. Scissors beats Paper');
+    } else {
+        console.log('You win. Rock beats Scissors.');
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports.playGame = playGame;
 module.exports.options = options;
